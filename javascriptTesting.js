3aeb6057924d526dcfb20407d64e45ae2de90d18
Will Mora <moranwilliam945@gmail.com>
2018-09-28 21:44:11 +0100

    testign

--- javascriptTesting.js
+++ javascriptTesting.js
@@ -1,4 +1,6 @@
-function changeText(){
+function changeText(){//this is a funtion to chaneg the center text when the button is clicked
     document.getElementById("showDT").innerHTML="well done you clicked the button but you got pranked";
+    var doe; var cheese;
+    doe = "one-hundred" 
 }
- 
+
