54daec10ee8666476ec0a10408b0702440438bb5
will <willmoran6969@gmail.com>
2018-11-18 15:27:56 +0000

    commite

--- scripts/javascriptTesting.js
+++ scripts/javascriptTesting.js
@@ -1,15 +1,5 @@
-function changeText(){//this is a funtion to chaneg the center text when the button is clicked
-    document.getElementById("showDT").innerHTML="well done you clicked the button but you got pranked";
-    var doe; var cheese;
-    doe = "one-hundred";
-}
-var colour = prompt("enter a colour to set the backgroudn to")
-var car = {
-colour:"pink",
-wheelSize:"12px",
-full : function(){
-    return this.colour + " " + this.wheelSize;
+function changeTextWithId(id ,text) {
+    document.getElementById(id).innerHTML=text;
 }
 
-};
-document.write(car.full()) 
+document.write(stringLengthTest);
