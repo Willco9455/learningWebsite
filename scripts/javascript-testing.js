f76c226ec346d2ea1a2f23f47aab0a59f616436c
will <willmoran6969@gmail.com>
2019-06-23 23:50:16 +0100

    testing some js

--- scripts/javascript-testing.js
+++ scripts/javascript-testing.js
@@ -1,1 +1,10 @@
-document.write("seventeen")
+function changeToRed (idToChange){
+    document.getElementById(idToChange).style.color="red";
+    document.getElementById("button1").style.display="none";
+    document.getElementById("button2").style.display="inline-block";
+}
+function changeToPink (idToChange){
+    document.getElementById(idToChange).style.color="pink";
+    document.getElementById("button1").style.display="inline-block";
+    document.getElementById("button2").style.display="none";
+}
